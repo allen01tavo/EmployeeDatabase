e3284c7142095cc04c5abe99309bdd0bd2170a3a
allen01tavo <gumaturana@gmail.com>
2022-09-14 23:36:50 -0400

    Update email.py
    Added updates to send emails

--- email.py
+++ email.py
@@ -1,14 +1,15 @@
 '''
-Created on August 26, 2022
-filename: database.py
-@author: Gus_Maturana
+    Created on August 26, 2022
+    filename: database.py
+    @author: Gus_Maturana
 '''
 
 from appscript import app, k
 from tkinter import filedialog
 import tkinter as ttk 
+import os
+import errors as ers 
 
-# 
 class email:
 
     def __init__(self):
@@ -17,20 +18,27 @@ class email:
 
     def send_email(self, email_, name_, subject_):
 
-        outlook = app('Microsoft Outlook')
+        files = ['Pixie_Cat_Phone_Screen_Form.xlsx']
+        for file in files:
+            with open(file, 'rb') as f:
+                file_data = f.read()
+                ##file_tye = imaghdr.what(f.name)
+                file_name = f.name
 
+        outlook = app('Microsoft Outlook')
         msg = outlook.make(
             new=k.outgoing_message,
             with_properties={
                 k.subject: subject_,
-                k.plain_text_content: 'Test email body'})
+                k.plain_text_content: 'Test email body',
+                k.attachment:file})
 
         msg.make(
             new=k.recipient,
             with_properties={
                 k.email_address: {
-                    k.name: name_,
-                    k.address: email_ }})
+                k.name: name_,
+                k.address: email_}})
 
         msg.open()
         msg.activate()
@@ -52,22 +60,40 @@ class messages:
         message = " Please schedule virtual inteview with " + name_ + " for " + date_ + " at " + time_
         return message
 
-    def browse_file(self):
+    def browse_file(self, newFolder_):
 
-        fname = filedialog.askopenfilename(filetypes = (("Template files", "*.type"), ("All files", "*")))
-        print (frame)
+        fname = filedialog.askopenfilename(filetypes = (("Template files", "*.*"), ("All files", "*")))
+        if fname != '':
+            extension = fname.split('.')[1]
+            print(extension)
 
-        root = Tk.Tk()
-        root.wm_title("Browser")
-        broButton = Tk.Button(master = root, text = 'Browse', width = 6, command=browse_file)
-        broButton.pack(side=Tk.LEFT, padx = 2, pady=2)
+            cmd = 'mkdir -p ' + str(newFolder_)
+            os.system(cmd)
+            cmd = 'cp ' + fname + ' ' + str(newFolder_)
+            os.system(cmd)
 
-        Tk.mainloop()
-
+    def open_files(self, key_):
+        path = str(key_)
+        print(path)
+        if os.path.exists(path):
+            list_files = os.listdir(path)
+            for i in list_files:
+                extension = i.split('.')[1]
+                if (extension == 'doc') or (extension == 'docx') or (extension == 'pdf'):
+                    cmd = 'open ' + path + '/' + i
+                    print(cmd)
+                    os.system(cmd)
+        else:
+            ers.errors().error_messages(11)
 
 def main():
     #email().send_email('gumaturana@gmail.com', 'Gustavo Maturana', 'Phone Screen with L3Harris')
-    messages().browse_file()
+    root = ttk.Tk()
+    root.wm_title("Browser")
+    broButton = ttk.Button(master = root, text = 'Browse', width = 6, command=lambda:messages().browse_file('Test'))
+    broButton.pack(side=ttk.LEFT, padx = 2, pady=2)
+
+    ttk.mainloop()
 
 
 if __name__ == '__main__':
